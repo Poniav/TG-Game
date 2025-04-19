bf32a507daefa3d77a5285fe3aeae1dd5ba27fe4
Guillaume G <guigarciafr@gmail.com>
2025-04-20 01:35:46 +0200

    handle disable swipe resize

--- src/hooks/useTelegramApp.tsx
+++ src/hooks/useTelegramApp.tsx
@@ -61,7 +61,7 @@ export function useTelegramWebApp(): TelegramWebAppHookResult {
       setColorScheme(tg.colorScheme || "light");
 
       if (isMobileUser) {
-        // tg.expand();
+        tg.expand();
         tg.disableVerticalSwipes();
         if (tg.isVersionAtLeast("8.0")) {
           tg.requestFullscreen();
@@ -95,6 +95,7 @@ export function useTelegramWebApp(): TelegramWebAppHookResult {
 
       if (newIsMobile && tg) {
         tg.expand();
+        tg.disableVerticalSwipes();
       }
     };
 
