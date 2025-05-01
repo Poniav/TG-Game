34c62b2f0d6f4c7cabf055edfd0d8905c5d3afed
Guillaume G <guigarciafr@gmail.com>
2025-05-01 23:34:02 +0200

    Update tasks

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -30,12 +30,44 @@ const Tasks = () => {
           paddingTop: `${areaInsets.top}px`,
         }}
       >
+        {/* Header */}
         <div className="flex flex-col items-center justify-center text-white mb-4">
-          <h1 className="text-2xl font-bold stroke-effect">Do your tasks</h1>
+          <h1 className="text-2xl font-bold stroke-effect">Daily tasks</h1>
           <p className="text-xs stroke-effect-small">
             and earn points to get rewards
           </p>
         </div>
+        {/* List 1 */}
+        <ul className="w-full flex flex-col gap-2">
+          {tasks.map((task) => (
+            <li
+              key={task.id}
+              className="flex items-center rounded-lg p-2 bg-white"
+            >
+              <div className="flex items-center justify-between w-full">
+                <div className="flex items-center gap-3">
+                  <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center border-2 border-black">
+                    <img src={task.icon} alt={task.alt} className="w-6 h-6" />
+                  </div>
+                  <div className="flex flex-col justify-between">
+                    <span className="text-sm inter-font">{task.name}</span>
+                    <span className="text-xs inter-font text-green-500 font-bold">
+                      +{task.points} pts
+                    </span>
+                  </div>
+                </div>
+                <div className="ml-auto">
+                  <BtnPrimary text="Claim" color="green" link={task.link} />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {/* Separator */}
+        <div className="flex flex-col items-center justify-center text-white mt-4 mb-4">
+          <h1 className="text-2xl font-bold stroke-effect">Partners rewards</h1>
+        </div>
+        {/* List 2 */}
         <ul className="w-full flex flex-col gap-2">
           {tasks.map((task) => (
             <li
